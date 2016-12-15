ada3d10a75578916e982e5bed79707c0c095cb13
digitalgaz <digitalgaz@hotmail.com>
2016-12-15 10:49:05 +0000

    Updated control to use library

--- controls/example.rb
+++ controls/example.rb
@@ -2,19 +2,20 @@
 # copyright: 2015, The Authors
 # license: All rights reserved
 
-title 'sample section'
+title 'determine last patch date'
 
-# you can also use plain tests
-describe file('/tmp') do
-  it { should be_directory }
-end
-
-# you add controls here
-control 'tmp-1.0' do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title 'Create /tmp directory'             # A human-readable title
-  desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
-    it { should be_directory }
+control 'os-patch-date-1.0' do
+  impact 0.5
+  title 'Check OS for patching date'
+  desc 'This test will check linux patching within 30 days, and windows within 90'
+  if os[:family] == 'redhat'
+    describe patch_date do
+      its('linux_within_30_days') { should be <= 30 }
+    end
+  end
+  if os[:family] == 'windows'
+    describe patch_date do
+      its('windows_within_90_days') { should be <= 90 }
+    end
   end
 end
